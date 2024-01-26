84eda527f9172a7c614af36ac120b9c3397d1ee3
vkzy <vkzya@LAPTOP-V2LRBOMP>
2024-01-26 08:44:03 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,28 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio.Contratos;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ControleDeContatos.Repositorio
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+        public ContatoRepositorio(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            _bancoContext.Contatos.Add(contato);
+            _bancoContext.SaveChanges();
+            return contato;
+        }
+
+        public List<ContatoModel> Buscar()
+        {
+            return _bancoContext.Contatos.ToList();
+        }
+    }
+}
---
+++ ControleDeContatos/Repositorio/Contratos/IContatoRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using ControleDeContatos.Models;
+using System.Collections.Generic;
+
+namespace ControleDeContatos.Repositorio.Contratos
+{
+    public interface IContatoRepositorio
+    {
+        ContatoModel Adicionar(ContatoModel contato);
+        List<ContatoModel> Buscar();
+    }
+}
